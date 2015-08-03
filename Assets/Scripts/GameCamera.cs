21e3b9e75590428c6667ca9e8c355950c97ab969
Those45Ninjas <tom.parker1235@gmail.com>
2015-08-03 12:26:29 +0800

    Better camera and few movement fixes
    Separated the camera from the player prefab, created a GameCamera script
    and changed the move script to move the rigidbody instead of the
    transform.
    Also changed the project to use Visible Meta Files.

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour
+{
+    #region Instace Stuff
+    private static GameCamera sceneInstance;
+    public static GameCamera instance
+    {
+        get
+        {
+            if(sceneInstance == null)
+            {
+                sceneInstance = FindObjectOfType<GameCamera>();
+                if(sceneInstance == null)
+                {
+                    Debug.LogWarning("Woah!, you need at least one GameCamera script in your scene. did you add a camera prefab?");
+                }
+            }
+            return sceneInstance;
+        }
+    }
+    #endregion
+
+    public float distance = 5f;
+    public float height = 0.5f;
+    public float angle = 190f;
+    public float cameraRollAmmount = 5f;
+    public float cameraRollDamp = 0.5f;
+
+    float cameraRollVelocity;
+    float cameraRoll;
+
+    Vector3 wantedPos;
+    Transform target;
+
+    Vector3 lastWantedPos;
+
+    void LateUpdate()
+    {
+        lastWantedPos = wantedPos;
+        if (target != null)
+            wantedPos = target.position;
+
+        wantedPos.y = height;
+
+        transform.position = wantedPos + Quaternion.Euler(angle, 0, 0) * Vector3.forward * distance;
+
+        float horizontalVelocity = wantedPos.x - lastWantedPos.x;
+
+        cameraRoll = Mathf.SmoothDamp(cameraRoll, horizontalVelocity * cameraRollAmmount, ref cameraRollVelocity, cameraRollDamp * Time.deltaTime);
+
+        transform.rotation = Quaternion.LookRotation((wantedPos - transform.position).normalized, Quaternion.Euler(0, 0, cameraRoll) * Vector3.up); ;
+    }
+
+    public void SetTarget(Transform newTarget)
+    {        
+        target = newTarget;
+        wantedPos = target.position;
+        wantedPos.y = height;
+        lastWantedPos = wantedPos;
+        Debug.Log(string.Format("Game Camera's target has been changed to <i>{0}</i>",newTarget.name));
+    }
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,11 +2,29 @@
 using System.Collections;
 
 public class Move : MonoBehaviour {
-	
+
+    [Tooltip("The speed (M/s) that the player moves along it's X axis (sideways)")]
 	public float speed = 1.0f;
-	
-	void Update() {
-		var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		transform.position += move * speed * Time.deltaTime;
-	}
+    [Tooltip("The speed that the player moves along it's Z axis (forward)")]
+    public float fwdSpeed = 5f;
+
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        // Get the rigidbody component and stuff in-to the rigidbody varible
+        // then tell the camera to follow this player
+
+        rigidbody = GetComponent<Rigidbody>();
+        GameCamera.instance.SetTarget(transform);
+    }
+
+    void FixedUpdate()
+    {
+        // Create a vector for the direction we want to move. then translate
+        // the rigidbody by the direction vector multiplied by time.delta time.
+
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, fwdSpeed);
+        rigidbody.MovePosition(rigidbody.position + move * Time.deltaTime);
+    }
 }
