4bb3824b0f17125361685192e862bb0aff3abe44
alexkirwan11 <alexkirwan29@gmail.com>
2015-08-22 17:10:03 +0800

    made a level browser yay

--- Unity Project/Assets/Scripts/UI/TestListItem.cs
+++ Unity Project/Assets/Scripts/UI/TestListItem.cs
@@ -6,32 +6,32 @@ using System;
 
 namespace UI
 {
-    public class TestListItem : MonoBehaviour
-    {
-        public Text title;
-        public Text description;
-        public RawImage image;
-        public Text rating;
-        public Text playCount;
-        public Text author;
-        public Texture2D defaultImage;
-        public void FillContents(JsonTests.ListElement data)
-        {
-            title.text = data.title;
-            description.text = data.description;
-            rating.text = string.Format("Rating {0}/5", data.rating);
-            playCount.text = string.Format("Plays {0}", data.playCount);
-            author.text = string.Format("Author: {0}", data.author);
-            StopAllCoroutines();
-            StartCoroutine(SetImage(data.imageURL));
-        }
-
-        private IEnumerator SetImage(string imageURL)
-        {
-            image.texture = defaultImage;
-            WWW www = new WWW(imageURL);
-            yield return www;
-            image.texture = www.texture;
-        }
-    }
+	public class TestListItem : MonoBehaviour
+	{
+		public Text title;
+		public Text description;
+		public RawImage image;
+		public Text rating;
+		public Text playCount;
+		public Text author;
+		public Texture2D defaultImage;
+		public void FillContents(JsonTests.ListElement data)
+		{
+			title.text = data.title;
+			description.text = data.description;
+			rating.text = string.Format("Rating {0}/5", data.rating);
+			playCount.text = string.Format("Plays {0}", data.playCount);
+			author.text = string.Format("Author: {0}", data.author);
+			StopAllCoroutines();
+			StartCoroutine(SetImage(data.imageURL));
+		}
+		
+		private IEnumerator SetImage(string imageURL)
+		{
+			image.texture = defaultImage;
+			WWW www = new WWW(imageURL);
+			yield return www;
+			image.texture = www.texture;
+		}
+	}
 }
