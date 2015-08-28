c7629390c9859cd6d864e571ef77d912c068dabc
Alex Kirwan <alexkirwan29@gmail.com>
2015-08-28 23:49:12 +1000

    more little changes :left_luggage:

---
+++ Unity Project/Assets/Scripts/wallDetect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallDetect : MonoBehaviour {
+
+    public float leftMost;
+    public float rightMost;
+
+	// Use this for initialization
+	void Start () {
+
+        RaycastHit hit;
+        float distanceToGround = 0;
+
+        if (Physics.Raycast(transform.position, Vector3.left, out hit))
+            distanceToGround = hit.distance;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
