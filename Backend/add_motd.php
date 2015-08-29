6e43b934c4bc0ac75923d7c6d7093b6435a4ded6
Those45Ninjas <tom.parker1235@gmail.com>
2015-08-29 10:08:10 +0800

    Improved the formatting that the add_motd script outputs

--- Backend/add_motd.php
+++ Backend/add_motd.php
@@ -9,7 +9,7 @@ if(isset($_GET['motdtext']) && isset($_GET['motdttl']))
     // Get the data from the url and put it in to some varibles
     $motd = $_GET['motdtext'];
     $ttl = $_GET['motdttl'];
-    
+
     // Open up a file and get it's contents then close it.
     $file = file_get_contents("motd.json");
     $data = json_decode($file);
@@ -19,16 +19,19 @@ if(isset($_GET['motdtext']) && isset($_GET['motdttl']))
     $data[] = array('motd'=>$motd,'ttl'=>$ttl);
     
     // Stuff the data into a file that overrided the starting file
-    file_put_contents("motd.json", json_encode($data));
+    file_put_contents("motd.json", json_encode($data, JSON_PRETTY_PRINT));
     unset($data);
     
     
-    // Retun some message.
-    echo "Success? I think it worked";
+    // Retun some messages.
+    echo "<h2>Success? I think it worked</h2>";
+    echo "<p>Your MOTD will last for $ttl seconds (that can be changed in game) and says<br>$motd</p>";
+    echo "<p><a href=\"motd.json\">Click to view the motd.json file</a></p>";
 }
 else
 {
     // Tell us that it failed!
-    echo "Failed! No data was sent!";
+    echo "<h2>Failed! No data was sent!</h2>";
+    echo "<p>you might have typed a string that caused an error... Check the url after the ?motdtext=</p>";
 }
 ?>
