a9ae71390233f158130eba42f8d68714c1b5e1b0
Alex Kirwan <alexkirwan29@gmail.com>
2015-08-29 20:33:29 +1000

    changed some visuals :yellow_heart:

---
+++ Unity Project/Assets/Scripts/webLink.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class webLink : MonoBehaviour {
+
+    public string URL;
+
+	// Use this for initialization
+	void Start () {
+        Application.OpenURL(URL);
+	}
+	
+
+}
