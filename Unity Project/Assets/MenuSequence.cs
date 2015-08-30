ba796d8d3258fd6d24bccf998cebce43baf395c8
Those45Ninjas <tom.parker1235@gmail.com>
2015-08-30 23:26:57 +0800

    created the a menu from the prototype

---
+++ Unity Project/Assets/MenuSequence.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cow.UI;
+using Cow.UI.Transitions;
+using UnityEngine.Events;
+
+namespace Cow.UI
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class MenuSequence : MonoBehaviour
+    {
+        [System.Serializable]
+        public class Slide
+        {
+            public Transition transition;
+            public float transitionDurationMull;
+            public float slideDuration;
+            public bool canSkip;
+
+            public RectTransform slideTransform;
+        }
+
+        public Slide[] slides;
+        int currSlide = 0;
+        CanvasGroup canvasGroup;
+
+
+        void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            foreach (Slide slide in slides)
+                slide.slideTransform.gameObject.SetActive(false);
+
+            CloseSlide(-1);
+        }
+
+        public void NextSlide()
+        {
+            slides[currSlide].transition.Enter(new UnityAction(delegate { CloseSlide(currSlide); }), slides[currSlide].transitionDurationMull);
+        }
+        public void CloseSlide(int index)
+        {
+            if(index >= 0)
+                slides[index].slideTransform.gameObject.SetActive(false);
+
+            currSlide = index + 1;
+            if (currSlide < slides.Length)
+            {
+                slides[currSlide].transition.Exit(null, slides[currSlide].transitionDurationMull);
+                slides[currSlide].slideTransform.gameObject.SetActive(true);
+                Invoke("NextSlide", slides[currSlide].slideDuration);
+            }
+            else
+            {
+                slides[slides.Length - 1].transition.Exit(null, slides[slides.Length - 1].transitionDurationMull);
+
+                canvasGroup.blocksRaycasts = false;
+            }
+        }
+    }
+}
