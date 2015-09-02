486e68fe71b0a8891ba09de08f82392bfa1b0c7b
Alex Kirwan <alexkirwan29@gmail.com>
2015-09-02 22:33:43 +1000

    made some pretty :princess:

---
+++ Unity Project/Assets/Scripts/cameracycle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameracycle : MonoBehaviour {
+
+    public KeyCode key;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(key))
+            GameCamera.instance.SetTarget(transform);
+
+
+    }
+}
+
