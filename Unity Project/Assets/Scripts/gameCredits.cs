6cfe0306451a59e75d5c28f31b9593ff031687f5
Alex Kirwan <alexkirwan29@gmail.com>
2015-09-06 00:26:22 +1000

    made a credits scene :lollipop:

---
+++ Unity Project/Assets/Scripts/gameCredits.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameCredits : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 1, 0));
+		
+	}
+}
