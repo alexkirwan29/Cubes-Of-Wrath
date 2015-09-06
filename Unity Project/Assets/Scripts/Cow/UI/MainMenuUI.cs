4168a5e43fee4f8774776e67bddc13c75008023d
Those45Ninjas <tom.parker1235@gmail.com>
2015-09-06 15:53:07 +0800

    Fixed the mainMenu. MOAR FEATURES!  Cleaned up all that done in 15 minutes!

---
+++ Unity Project/Assets/Scripts/Cow/UI/MainMenuUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Cow.UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        public void LoadLevel(int scene)
+        {
+            Application.LoadLevel(scene);
+        }
+        public void LoadLevel(string name)
+        {
+            Application.LoadLevel(name);
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/ProtoMenuCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ProtoMenuCamera : MonoBehaviour
+{
+    public int size = 25;
+    public float dist = 2f;
+
+    Vector3 targetPos;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        targetPos = new Vector3(Random.Range(-size, size), 1, Random.Range(-size, size));
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(targetPos);
+    }
+
+    void Update()
+    {
+        if(agent.remainingDistance < dist && !agent.pathPending)
+        {
+            targetPos = new Vector3(Random.Range(-size, size), 1, Random.Range(-size, size));
+            agent.SetDestination(targetPos);
+        }
+    }
+}
