f90085926496faf38e63d050096b546701a2df36
Makammo <mackiebailye@gmail.com>
2015-09-20 11:06:06 +0800

    Ads. That is all.
    Forget the gui button, thats temporary

---
+++ Unity Project/Assets/Scripts/Cow/UI/Showad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Showad : MonoBehaviour
+{
+    public void Awake()
+    {
+        if (Advertisement.isSupported)
+        {
+            Advertisement.Initialize("76230");
+        }
+        else
+        {
+            Debug.Log("Platform not supported");
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 300, 125), Advertisement.IsReady() ? "Show Ad" : "Waiting..."))
+        {
+            // Show with default zone, pause engine and print result to debug log
+            Advertisement.Show(null, new ShowOptions
+            {
+                resultCallback = result => {
+                    Debug.Log(result.ToString());
+                }
+            });
+        }
+    }
+}
