c1729f84e1bdb6ab38733a128caaa12c65135cc8
Alex Kirwan <alexkirwan29@gmail.com>
2015-10-04 19:47:19 +0800

    made some propagana

---
+++ Unity Project/Assets/Scripts/StartTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartTimer : MonoBehaviour {
+
+    public float timeLeft = 3;
+    Text startTimer;
+
+    void Update ()
+    {
+        timeLeft -= Time.deltaTime;
+
+        {
+            startTimer = GetComponent<Text>();
+            startTimer.text = string.Format("{1:0}", (int)(timeLeft / 60), timeLeft % 60);
+        }
+
+        if (timeLeft < 0)
+        {
+            Debug.Log("the timer fuken finshed. k");
+            
+            
+        }
+                
+
+    }    
+           
+
+}
