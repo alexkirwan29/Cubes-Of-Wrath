f4c9678f3de69f9177ede1d9bc26a537dbb1e7f4
Alex Kirwan <alexkirwan29@gmail.com>
2015-11-01 19:59:37 +0800

    more menu stuff... now with pics :paperclip:

---
+++ Unity Project/Assets/Scripts/Cow/UI/wwwImage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class wwwImage : MonoBehaviour {
+
+	public string url = "http://www.imagelocation.com";
+
+	IEnumerator Start() {
+		//Start download of the given URL
+		WWW www = new WWW (url);
+
+		//Wait for download to complete
+		yield return www;
+
+		// assign texture
+
+		RawImage image = GetComponent<RawImage> ();
+		image.texture = www.texture;
+	}
+}
