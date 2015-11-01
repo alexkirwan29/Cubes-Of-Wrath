0d6d7e21a6ada74c8915e4f162d42e891f97d0e8
Those45Ninjas <tom.parker1235@gmail.com>
2015-11-01 20:15:36 +0800

    Removed the advert plugin(s) and fixed the news.

--- Unity Project/Assets/Scripts/Cow/UI/newmotd.js
+++ Unity Project/Assets/Scripts/Cow/UI/newmotd.js
@@ -1,11 +1,9 @@
-﻿var url = "203.143.87.76/motd.html"; var guiText1 : UnityEngine.UI.Text;
-
-  function Start () {
-      var guiwww : WWW = new WWW(url);
-      yield guiwww;
+﻿@script RequireComponent(UnityEngine.UI.Text)
+var url = "203.143.87.76/motd.html";
+function Start ()
+{
+    var guiwww : WWW = new WWW(url);
+    yield guiwww;
  
-      guiText1.text = guiwww.text;
+    GetComponent(UnityEngine.UI.Text).text = guiwww.text;
 }
-
-
-
