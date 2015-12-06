88db853cc2a609eed82ad716e8777ed8570b5b9d
Makammo <mackiebailye@gmail.com>
2015-12-06 12:08:33 +0800

    enemy shoots
    move the cube. dare ya

---
+++ Unity Project/Assets/Scripts/BulletAlive.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletAlive : MonoBehaviour {
+    float bulletAlive = 1.0f;
+	void Update () {
+        Destroy(gameObject, bulletAlive);
+	}
+}
---
+++ Unity Project/Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBullet : MonoBehaviour {
+    public Rigidbody projectile;
+    public float nextShot = 0;
+    public float fireDelay = 0.5f;
+    void Update()
+    {
+        if (Time.time > nextShot)
+        {
+            nextShot = Time.time + fireDelay;
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    }
+ }
