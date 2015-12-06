b14db57f5c7cf78d7c65fe8de51e9681682b171c
Alex Kirwan <alexkirwan29@gmail.com>
2015-12-06 09:35:01 +0800

    cyuj

--- Unity Project/Assets/Scripts/Game/startTimer.cs
+++ Unity Project/Assets/Scripts/Game/startTimer.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
+public class startTimer : MonoBehaviour
+{
+
+    public float timeLeft = 600;
+    Text countdownText;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        {
+            countdownText = GetComponent<Text>();
+            countdownText.text = string.Format("Time Left:  {0}.{00:00.00}", (int)(timeLeft / 60), timeLeft % 60);
+        }
+
+        if (timeLeft < 0)
+        {
+            countdownText.text = (" sfdg");
+        }
+    }
 
 
-public class startTimer : MonoBehaviour {
 
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
