f66700193a75399fb10c892de57a361ebc8d1388
Alex Kirwan <alexkirwan29@gmail.com>
2015-12-06 12:07:26 +0800

    Making floor material

---
+++ Unity Project/Assets/Scripts/levelEnd/calcScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class calcScore : MonoBehaviour {
+
+    public int voxelamnt;  //ammount of voxel collected this level
+    public int mainscore;  //score achieved in played level
+    private int ovrltime = 100;   //time taken to complete level
+
+
+	// Use this for initialization
+	void Start () {
+	
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
